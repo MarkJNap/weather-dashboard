0062e5f82dc253534a0af968feb7e1312d48c020
Mark Napolitano <156marknap@gmail.omgit config --global user.name oMarkNx>
2023-01-03 15:47:28 +0800

    Addded jQuery and added the search to be listed, moved the api calling into functions and made the submit work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,45 @@
 const apiKey = "2d3402229563fbfc5f169c939d8dc026";
+let city = "London";
 
-// API Call example:
-// http://api.openweathermap.org/geo/1.0/direct?q=London&appid={API key}
-
-
-// Call for 5 day forecast
-requestForecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=Perth&appid=" + apiKey + "&units=metric"
-
-function getApi(requestForecastUrl) {
-    fetch(requestForecastUrl)
+$(function () {
+  // Function for getting the 5 Day forecast
+function getForecast(city) {
+  requestForecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=metric";
+  fetch(requestForecastUrl)
       .then(function (response) {
         return response.json()
     })
     .then(function (data) {
         console.log(data);
+        // console.log(data.list[0].main.temp);
     });
   }
 
-getApi(requestForecastUrl);
+// Function for getting the current weather 
+function getCurrentWeather(city) {
+  requestCurrentUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+  fetch(requestCurrentUrl)
+    .then(function (response) {
+      return response.json()
+  })
+  .then(function (data) {
+      console.log(data);
+      //Adds the searched city to the recent searches list
+      let newCity = $("<li>").text(city)
+      $("#recent-cities").append(newCity)
+  });
+}
+// When a city is submitted it runs needed functions
+$("#city-search-form").on("submit", function(e) {
+  e.preventDefault();
+  let city = $("#city").val();
+  getForecast(city);
+  getCurrentWeather(city);
+})
+
+
+
+
+
+})
+
